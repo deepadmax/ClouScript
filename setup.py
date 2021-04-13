3dacd25179bcd19ae0ccfedf34aafceac7179ec3
deepadmax <maximillian.strand@gmail.com>
2021-04-13 03:09:53 +0200

    added setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name='ClouScript',
+    url='https://github.com/deepadmax/ClouScript',
+    author='Maximillian Strand',
+    author_email='maximillian.strand@gmail.com',
+    packages=['clouscript'],
+    install_requires=[],
+    version='0.1',
+    license='GPLv3',
+    description='A Python library enabling you to set up your own very basic yet powerful programming language.',
+    long_description=open('README.md').read(),
+)
